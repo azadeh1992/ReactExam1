dd07717de1b5660c3dce6fc2c9bcb35aae89e744
Azadeh Galidari <azadeh.galidari.1992@gmail.com>
2020-05-12 13:54:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Validation from "./Components/validation";
+import Char from "./Components/char";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userInput: "",
+  };
+
+  inputChangeHandler = (event) => {
+    let userInputS = { ...this.state.userInput };
+    userInputS = event.target.value;
+
+    this.setState({ userInput: userInputS });
+    // console.log(userInputS);
+  };
+
+  deleteCharHandler = (index) => {
+
+  }
+
+  render() {
+    const charList = this.state.userInput.split("").map((ch, index) => {
+      return <Char character={ch} key={index} clicked={() => this.deleteCharHandler(index)} />
+    })
+
+    return (
+      <div className="App">
+        <input
+          type="text"
+          onChange={this.inputChangeHandler}
+          value={this.state.userinpute}
+        />
+        <p> {this.state.userInput} </p>
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
+      </div>
+    );
+  }
 }
 
 export default App;
